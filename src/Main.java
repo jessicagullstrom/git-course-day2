294600893a4281dcc0142145e6144de3d4bc51cb
jessicagullstrom <jessica.gullstrom@hm.com>
2021-02-05 11:16:04 +0100

    Subtrahera

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,22 @@ public class Main {
     public static void main(String[] args) {
         int num = 10;
         int increment = increment(num);
+        int decrement = decrement(increment);
+
         System.out.println(increment);
+        System.out.println(decrement);
     }
+
 
     private static int increment(int number) {
         return ++number;
+    }
+
+        private static int decrement(int number) {
+            return --number;
+        }
 
 
 
     }
-}
+
