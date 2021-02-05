08919efb67653cfa99bfbc01e7bc236f0cda6567
jessicagullstrom <jessica.gullstrom@hm.com>
2021-02-05 10:20:55 +0100

    Nyr Ãrättad main

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
-	int num =10;
-	int increment = increment (num);
+        int num = 10;
+        int increment = increment(num);
+        System.out.println(increment);
     }
 
     private static int increment(int number) {
         return ++number;
+
+
+
     }
 }
