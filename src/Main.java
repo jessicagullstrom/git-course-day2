30cf2a300f3a05886ecc1112311898ff7e10a397
jessicagullstrom <jessica.gullstrom@hm.com>
2021-02-05 10:03:52 +0100

    NyMain

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+	int num =10;
+	int increment = increment (num);
+    }
+
+    private static int increment(int number) {
+        return ++number;
+    }
+}
